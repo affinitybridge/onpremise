e7164a8051d520a17a40f5cafd94158a470cea7a
Affinity Bridge <affinity@git.affinitybridge.com>
2017-06-24 00:12:18 -0700

    Switching to Docker user defined networks instead of deprecated --link. Switching to Postfix image instead of other SMTP thingy

--- sentry-init.sh
+++ sentry-init.sh
@@ -12,9 +12,13 @@ fi
 # Create Docker container (image?).
 make build
 
+# Create Docker bridge network to use.
+docker network create sentry-network
+
 # Create dependant services for Sentry.
 docker run \
    --detach \
+   --network sentry-network \
    --env POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
    --env POSTGRES_USER=${POSTGRES_USER} \
    --name sentry-postgres \
@@ -22,28 +26,28 @@ docker run \
 
 docker run \
    --detach \
+   --network sentry-network \
    --name sentry-redis \
    redis:3.2-alpine
 
 docker run \
    --detach \
+   --env POSTFIX_myhostname=sentry.affinitybridge.com \
+   --network sentry-network \
    --name sentry-smtp \
-   tianon/exim4
+   mwader/postfix-relay
 
 # Pause to let services start.
 echo "Waiting 5 seconds for Postgres to start..."
 sleep 5
 
-# Setting some helper variables.
-SENTRY_SERVICES="\
-   --link sentry-redis:redis \
-   --link sentry-postgres:postgres \
-   --link sentry-smtp:smtp \
-"
-
 SENTRY_ENV="\
+   --env SENTRY_REDIS_HOST=sentry-redis \
+   --env SENTRY_POSTGRES_HOST=sentry-postgres \
+   --env SENTRY_DB_USER=${POSTGRES_USER} \
+   --env SENTRY_DB_PASSWORD=${POSTGRES_PASSWORD} \
    --env SENTRY_SERVER_EMAIL=sentry@sentry.affinitybridge.com \
-   --env SENTRY_EMAIL_HOST=localhost \
+   --env SENTRY_EMAIL_HOST=sentry-smtp \
    --env SENTRY_ENABLE_EMAIL_REPLIES=1 \
    --env SENTRY_SMTP_HOSTNAME=sentry.affinitybridge.com \
    --env SENTRY_AUTH_REGISTER=0 \
@@ -54,7 +58,7 @@ SENTRY_ENV="\
 
 # Start Sentry services.
 docker run \
-   ${SENTRY_SERVICES} \
+   --network sentry-network \
    ${SENTRY_ENV} \
    --rm \
    -it \
@@ -63,7 +67,7 @@ docker run \
 
 docker run \
    --detach \
-   ${SENTRY_SERVICES} \
+   --network sentry-network \
    ${SENTRY_ENV} \
    --publish 9000:9000 \
    --name sentry-web-01 \
@@ -72,7 +76,7 @@ docker run \
 
 docker run \
    --detach \
-   ${SENTRY_SERVICES} \
+   --network sentry-network \
    ${SENTRY_ENV} \
    --name sentry-worker-01 \
    ${REPOSITORY} \
@@ -80,11 +84,13 @@ docker run \
 
 docker run \
    --detach \
-   ${SENTRY_SERVICES} \
+   --network sentry-network \
    ${SENTRY_ENV} \
    --name sentry-cron \
    ${REPOSITORY} \
    run cron
 
+# List Docker networks.
+docker network ls
 # List running Docker processes.
 docker ps -a
